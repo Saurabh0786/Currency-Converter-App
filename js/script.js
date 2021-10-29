0c97023ddbbff3e0decb0c901a4b2b052bf8b860
Saurabh Shrivastav <40481420+Saurabh0786@users.noreply.github.com>
2021-10-29 09:50:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const dropList = document.querySelectorAll("form select"),
 fromCurrency = document.querySelector(".from select"),
 toCurrency = document.querySelector(".to select"),
 getButton = document.querySelector("form button");
-FF
+
 
 for (let i = 0; i < dropList.length; i++) {
     for(let currency_code in country_list){
